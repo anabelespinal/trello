d1e14e045608e0729bb5de60f693025bda476436
anbelespinal <anabelespinal>
2016-09-22 13:27:33 -0500

    terminando version 2

--- js/main.js
+++ js/main.js
@@ -1,27 +1,53 @@
 window.addEventListener("load", function() {
-
-    var tarjetas = document.getElementById("tarjetas");
-    var span = document.getElementById("span");
-    
-    span.addEventListener("click", function(e) {
-        e.preventDefault();
-        creandoTarjetas();
-    });
-
-    function creandoTarjetas() {
+var span = document.getElementById("span")
+ span.addEventListener("click", function(e) {
+    tarjetasNuevas(); 
+  })
+  function tarjetasNuevas(){
+    var formCreado = document.createElement("form");
+    var inputCreado = document.createElement("input");
+    var botonCreado = document.createElement("button");
+      function anadirTarjetas(){
         span.style.display = "none";
 
-        var formCreado = document.createElement("form");
         tarjetas.insertBefore(formCreado, tarjetas.childNodes[0]);
 
-        var inputCreado = document.createElement("input");
+        inputCreado.placeholder = "añadir una lista";
         formCreado.insertBefore(inputCreado, formCreado.childNodes[0]).classList.add("inputCreado");
 
-        var botonCreado = document.createElement("button");
         var texto = document.createTextNode("Guardar");
         botonCreado.appendChild(texto);
         formCreado.insertBefore(botonCreado, formCreado.childNodes[1]).classList.add("botonCreado");
 
         tarjetas.classList.add("contenedorCreado");
-    }
-});        
+      }
+
+      function botonGuardar(){
+        anadirTarjetas();
+        botonCreado.addEventListener("click",function(e){
+          e.preventDefault();
+
+          formCreado.style.display = "none";
+          tarjetas.removeAttribute("class","contenedorCreado")
+          var valueTarjetas = inputCreado.value;
+          var tarjetaNombre = document.createElement("span");
+          tarjetaNombre.setAttribute("class", "nombreTarjeta");
+          tarjetas.insertBefore(tarjetaNombre, tarjetas.childNodes[0]);
+          tarjetaNombre.innerText = valueTarjetas;
+          var newboton = document.createElement("button");
+          newboton.innerText = "añadir tarjeta";
+          newboton.setAttribute("class","buttonDos");
+          tarjetas.insertBefore(newboton, tarjetas.childNodes[1]);
+      });
+
+      }
+      botonGuardar();
+  };
+
+});     
+          // function nuevoanadir(tarjetasNuevas){
+          //   botonGuardar();
+          //   var nuevatarjeta = document.createElement("span");
+          //   nuevatarjeta.setAttribute("class", "span");
+          //   tarjetas.insertBefore(nuevatarjeta, tarjetas.childNodes[1])
+          // };   
