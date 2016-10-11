d683eeec503069568c0cc059734df2a0eba1e086
anbelespinal <anabelespinal>
2016-10-11 15:57:16 -0500

    avanzando drag and drop

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
-window.addEventListener("load", function(){
-  var contenedor = document.getElementById("contenedor");
+window.addEventListener("load", trello);
+
+ var ids = 1;
+function trello(){
   var anadirLista = document.getElementById("anadirLista");
+  var contenedor = document.getElementById("contenedor");
   var formCreado = document.getElementById("formCreado");
   var inputCreado = document.getElementById("inputCreado");
   var botonCreado = document.getElementById("botonCreado");
@@ -29,20 +32,20 @@ window.addEventListener("load", function(){
     anadirLista.parentElement.appendChild(nombreLista);
     anadirLista.parentElement.appendChild(anadirTarjeta);
     inputCreado.value = "";
+    var contenedorTarjetas = document.createElement("div");
+    anadirTarjeta.parentElement.insertBefore(contenedorTarjetas, anadirTarjeta.parentElement.children[3]);
+      contenedorTarjetas.setAttribute("class", "contenedorTarjetas");
 
     nuevoDiv();
 
     anadirTarjeta.addEventListener("click", function(){
       anadirTarjeta.style.display = "none";
-      var contenedorTarjetas = document.createElement("div");
-      anadirTarjeta.parentElement.appendChild(contenedorTarjetas);
-      contenedorTarjetas.setAttribute("class", "contenedorTarjetas");
       var newForm = document.createElement("form");
       contenedorTarjetas.appendChild(newForm);
       newForm.setAttribute("class","newForm")
       var textArea = document.createElement("textarea");
       newForm.appendChild(textArea);
-      textArea.focus();
+      textArea.focus();  
       textArea.setAttribute("class", "textArea")
       var boton = document.createElement("button");
       newForm.appendChild(boton);
@@ -57,8 +60,37 @@ window.addEventListener("load", function(){
         contenedorTarjetas.appendChild(newTarjeta);
         contenedorTarjetas.parentElement.appendChild(anadirTarjeta);
         newTarjeta.setAttribute("class", "newTarjeta");
+        newTarjeta.setAttribute("id", ids++);
+        newTarjeta.setAttribute("draggable", "true");
         anadirTarjeta.style.display = "inline-block";
-      })
+        function dragStart(event){
+          event.dataTransfer.setData("text", event.target.id);
+        };
+        function allowDrop(event){
+          event.preventDefault();
+        };
+        function drop(event){
+          event.preventDefault();
+          var data = event.dataTransfer.getData("text");
+          event.target.parentNode.appendChild(document.getElementById(data));
+          if (event.target.firstChild.nextSibling == null){
+            event.target.appendChild(document.getElementById(data));
+          }else if (event.target.firstChild.nextSibling != null ){
+            event.target.insertBefore(document.getElementById(data), event.target.children[1]);
+            alert("esto es null");
+          }
+        };
+
+        contenedorTarjetas.addEventListener("drop", function(event){
+          drop(event);
+        });
+        contenedorTarjetas.addEventListener("dragover", function(event){
+          allowDrop(event);
+        });
+        newTarjeta.addEventListener("dragstart", function(event){
+          dragStart(event);
+        });
+      });
     });
   });
 
@@ -70,4 +102,4 @@ window.addEventListener("load", function(){
     newDiv.appendChild(formCreado);
     anadirLista.style.display = "inline-block"
   }
-});
+}
